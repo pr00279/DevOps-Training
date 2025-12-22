ea9f0b368acfa068d2d300e8792bee116a9c87fc
P Revolta <phoebe.revolta@solirius.com>
2025-12-22 20:00:51 +0000

    module-3-lab: exclude directories in eslint config

--- src/azure-sa/eslint.config.mjs
+++ src/azure-sa/eslint.config.mjs
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import {defineConfig, globalIgnores} from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } }, globalIgnores(["**/jenkins_compose", ".pnp.cjs"])
 ]);
