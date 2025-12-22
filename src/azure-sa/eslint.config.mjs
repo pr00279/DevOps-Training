c6744b53b0b75c09495e0fdc9d4e0c16cd02ed58
P Revolta <phoebe.revolta@solirius.com>
2025-12-22 20:12:37 +0000

    module-3-lab: amend eslint config to accommodate node environment

--- src/azure-sa/eslint.config.mjs
+++ src/azure-sa/eslint.config.mjs
@@ -3,5 +3,5 @@ import globals from "globals";
 import {defineConfig, globalIgnores} from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } }, globalIgnores(["**/jenkins_compose", ".pnp.cjs"])
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: { ...globals.browser, ...globals.node } } }, globalIgnores(["**/jenkins_compose", ".pnp.cjs"])
 ]);
