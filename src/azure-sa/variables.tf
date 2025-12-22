820ba353d1b68edc93a798755d6f2447bdc650d4
P Revolta <phoebe.revolta@solirius.com>
2025-12-22 16:16:48 +0000

    module-9-lab: remove container_name from dockerfile, add terraform file for aws ecr deployment, add old azure app service terraform config, add variables file

---
+++ src/azure-sa/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+  ephemeral = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+  ephemeral = true
+}
